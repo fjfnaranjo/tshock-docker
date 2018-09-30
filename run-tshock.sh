dd8703da3857e3d181f64f4c74ec613e0d026d9b
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2018-09-30 14:38:49 +0200

    Improve run-tshock.sh script

--- run-tshock.sh
+++ run-tshock.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# chdir to script directory
+cd "$(dirname "$0")"
+
+# Ensure tShock service image and volumes
 docker-compose up --no-start
+
+# Run tShock CLI
 docker-compose run --rm --entrypoint "mono TerrariaServer.exe -configpath /var/tshock/config -worldpath /var/tshock/world -logpath /var/tshock/log" tshock
 
